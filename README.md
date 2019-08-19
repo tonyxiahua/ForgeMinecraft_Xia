57885317aeb2aac9a10aba322f73527c87d36524
tonyxiahua <rednik111@gmail.com>
2019-08-19 10:47:26 -0700

    auto backup server snapshot2019-08-19 10:47:26.872978

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ForgeMinecraft_Xia
+一个自动的Minecraft 服务器备份~
+
+添加记录push信息就好了
+而且利用python 来进行系统自动备份
+.git/config
+'''
+[credential]
+    helper = store
+'''
--- autoGithub.py
+++ autoGithub.py
@@ -3,5 +3,5 @@ import subprocess
 import datetime
 
 subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())]) # 加上当前系统的时间
+subprocess.call(["git", "commit", "-m", "auto backup server snapshot" + str(datetime.datetime.now())]) # 加上当前系统的时间
 subprocess.call(["git", "push"])
